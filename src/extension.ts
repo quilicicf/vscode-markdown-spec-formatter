655e2845dd4958e05e01ac5158b29ddd95b07b38
quilicicf <quilicicf@gmail.com>
2021-08-16 19:26:29 +0200

    :lock: Upgrade libs

--- src/extension.ts
+++ src/extension.ts
@@ -1,15 +1,15 @@
+import { VFile } from 'vfile';
 import * as vscode from 'vscode';
-import {formatFromString} from '@quilicicf/markdown-formatter';
+import { formatFromString } from '@quilicicf/markdown-formatter';
 
-export function activate() {
-    vscode.languages.registerDocumentFormattingEditProvider('markdown', {
-        provideDocumentFormattingEdits(document: vscode.TextDocument): vscode.TextEdit[] {
-            const fullText = document.getText();
-            return formatFromString(fullText)
-                .then(({contents: newText}) => {
-                    const fullRange = new vscode.Range(document.positionAt(0), document.positionAt(fullText.length));
-                    return [vscode.TextEdit.replace(fullRange, newText)];
-                });
-        }
-    });
+export function activate () {
+  vscode.languages.registerDocumentFormattingEditProvider('markdown', {
+    async provideDocumentFormattingEdits (document: vscode.TextDocument): Promise<vscode.TextEdit[]> {
+      const fullText = document.getText();
+      const virtualFile: VFile = await formatFromString(fullText);
+      const newText: string = virtualFile.value as string;
+      const fullRange = new vscode.Range(document.positionAt(0), document.positionAt(fullText.length));
+      return [ vscode.TextEdit.replace(fullRange, newText) ];
+    },
+  });
 }
