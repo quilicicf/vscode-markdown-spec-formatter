b0cfa80b3ac9c175a3f28b2f1486f84772341852
quilicicf <quilicicf@gmail.com>
2021-08-23 11:09:28 +0200

    :new: Add settings to configure the formatter
    BONUS: libs upgrade

--- PACKAGING.md
+++ PACKAGING.md
@@ -5,6 +5,6 @@
 * Update the changelog
 * Run `npm run package`
 * Install the vsix and try it with `code --install-extension markdown-spec-formatter-*.vsix` 
-* Uninstall the vsix and try it with `code --uninstall-extension markdown-spec-formatter-*.vsix`
+* Uninstall the vsix `code --uninstall-extension markdown-spec-formatter-*.vsix`
 * Publish the new vsix [on VSCode's website](https://marketplace.visualstudio.com/manage/publishers/quilicicf)
 * Tag this repository
--- src/extension.ts
+++ src/extension.ts
@@ -1,15 +1,29 @@
-import { VFile } from 'vfile';
+// Objects
 import * as vscode from 'vscode';
-import { formatFromString } from '@quilicicf/markdown-formatter';
+import formatFromString from '@quilicicf/markdown-formatter/lib/formatFromString.js';
 
-export function activate () {
+// Types
+import { VFile } from 'vfile';
+import { MarkdownFormatterOptions, RemarkStringifyOptions } from '@quilicicf/markdown-formatter/types/index';
+
+interface PluginOptions {
+  markdownFormatterOptions?: MarkdownFormatterOptions,
+  stringifyOptions?: RemarkStringifyOptions,
+}
+
+function loadSettings(): PluginOptions {
+  return vscode.workspace.getConfiguration('markdown-spec-formatter') as PluginOptions;
+}
+
+export function activate() {
   vscode.languages.registerDocumentFormattingEditProvider('markdown', {
-    async provideDocumentFormattingEdits (document: vscode.TextDocument): Promise<vscode.TextEdit[]> {
+    async provideDocumentFormattingEdits(document: vscode.TextDocument): Promise<vscode.TextEdit[]> {
+      const settings: PluginOptions = loadSettings();
       const fullText = document.getText();
-      const virtualFile: VFile = await formatFromString(fullText);
+      const virtualFile: VFile = await formatFromString(fullText, settings.markdownFormatterOptions || {}, settings.stringifyOptions || {});
       const newText: string = virtualFile.value as string;
       const fullRange = new vscode.Range(document.positionAt(0), document.positionAt(fullText.length));
-      return [ vscode.TextEdit.replace(fullRange, newText) ];
+      return [vscode.TextEdit.replace(fullRange, newText)];
     },
   });
 }
