07a017c27d37f90a9009b5dc7ec4058f77986ad2
quilicicf <quilicicf@gmail.com>
2022-11-02 13:39:36 +0100

    :lock: Dependencies upgrade

--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,34 @@
 // Objects
-import * as vscode from 'vscode';
 import formatFromString from '@quilicicf/markdown-formatter/lib/formatFromString.js';
 
 // Types
 import { VFile } from 'vfile';
-import { MarkdownFormatterOptions, RemarkStringifyOptions } from '@quilicicf/markdown-formatter/types/index';
+// eslint-disable-next-line import/no-unresolved
+import * as vscode from 'vscode';
+import { MarkdownFormatterOptions, RemarkStringifyOptions } from '@quilicicf/markdown-formatter';
 
 interface PluginOptions {
   markdownFormatterOptions?: MarkdownFormatterOptions,
   stringifyOptions?: RemarkStringifyOptions,
 }
 
-function loadSettings(): PluginOptions {
+function loadSettings (): PluginOptions {
   return vscode.workspace.getConfiguration('markdown-spec-formatter') as PluginOptions;
 }
 
-export function activate() {
+// eslint-disable-next-line import/prefer-default-export
+export function activate () {
   vscode.languages.registerDocumentFormattingEditProvider('markdown', {
-    async provideDocumentFormattingEdits(document: vscode.TextDocument): Promise<vscode.TextEdit[]> {
+    async provideDocumentFormattingEdits (document: vscode.TextDocument): Promise<vscode.TextEdit[]> {
       const settings: PluginOptions = loadSettings();
       const fullText = document.getText();
-      const virtualFile: VFile = await formatFromString(fullText, settings.markdownFormatterOptions || {}, settings.stringifyOptions || {});
+      const virtualFile: VFile = await formatFromString(
+        fullText,
+        settings.markdownFormatterOptions || {}, settings.stringifyOptions || {},
+      );
       const newText: string = virtualFile.value as string;
       const fullRange = new vscode.Range(document.positionAt(0), document.positionAt(fullText.length));
-      return [vscode.TextEdit.replace(fullRange, newText)];
+      return [ vscode.TextEdit.replace(fullRange, newText) ];
     },
   });
 }
